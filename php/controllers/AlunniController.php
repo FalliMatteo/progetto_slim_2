c35a5c1c159e461e0292eb593201efb44308e5c9
lai2-24 <lai2-24@itismeucci.com>
2024-04-09 14:21:10 +0200

    added getAlunni and postAlunni methods in AlunniController.php

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -4,12 +4,46 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class AlunniController
 {
-  public function index(Request $request, Response $response, $args){
-    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $result = $mysqli_connection->query("SELECT * FROM alunni");
-    $results = $result->fetch_all(MYSQLI_ASSOC);
+  public function connectMySQL(){
+    $connection = new mysqli("localhost", "root", "ciccio", "scuola");
+    if($connection->connect_error){
+        die($connection->connect_error);
+    }
+    return $connection;
+  }
 
+  public function index(Request $request, Response $response, $args){
+    $connection = connectMySQL();
+    $result = $connection->query("SELECT * FROM alunni");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function getAlunno(Request $request, Response $response, $args){
+    $connection = connectMySQL();
+    $id = $args["id"];
+    $result = $connection->query("SELECT * FROM alunni WHERE id = $id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function postAlunno(Request $request, Response $response, $args){
+    $connection = connectMySQL();
+    $data = json_decode($request->getBody()->getContents(), true);
+    $nome = $data["nome"];
+    $cognome = $data["cognome"];
+    $connection->query("INSERT INTO alunni (nome, cognome) VALUES($nome, $cognome)");
+    $response->getBody()->write(json_encode($data));
+    return $response->withHeader("Content-Type", "application/json")->withStatus(200);
+  }
+
+  public function putAlunno(Request $request, Response $response, $args){
+
+  }
+
+  public function deleteAlunno(Request $request, Response $response, $args){
+
   }
 }
--- php/index.php
+++ php/index.php
@@ -7,5 +7,9 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
+$app->get("/alunni/{id}", "AlunniController:getAlunno");
+$app->post("/alunni", "AlunniController:postAlunno");
+$app->put("/alunni/{id}", "AlunniController:putAlunno");
+$app->delete("/alunni/{id}", "AlunniController:deleteAlunno");
 
 $app->run();
